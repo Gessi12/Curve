78fb5e4be19b9d9ad04a9a4790b9f2eb6afcc1f3
Gessi12 <2846141630@qq.com>
2024-04-01 14:43:11 +0800

    update stake contract

--- contracts/Stake/usdt-crv-stake.sol
+++ contracts/Stake/usdt-crv-stake.sol
@@ -4,27 +4,29 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract StakingContract {
+    IERC20 public usdtToken;
     IERC20 public crvToken;
-    uint256 public exchangeRate;  // ETH-to-CRV exchange rate
+    uint256 public exchangeRate;
     uint256 public rewardRate;    // Reward rate as a percentage (e.g., 5 for 5%)
-    uint256 public totalStakedETH;
+    uint256 public totalStakedUSDT;
     uint256 public constant ONE_WEEK = 1 weeks;
     address public owner;
 
     struct Stake {
-        uint256 amount;
+        uint256 usdtAmount;
         uint256 timestamp;
     }
 
     mapping(address => Stake) public stakes;
 
-    event Staked(address indexed account, uint256 amount);
-    event Unstaked(address indexed account, uint256 amount);
-    event RewardClaimed(address indexed account, uint256 amount);
+    event Staked(address indexed account, uint256 usdtAmount);
+    event Unstaked(address indexed account, uint256 usdtAmount);
+    event RewardClaimed(address indexed account, uint256 crvAmount);
 
-    constructor(address _crvToken, uint256 _rewardRate) {
+    constructor(address _usdtToken, address _crvToken, uint256 _rewardRate) {
+        usdtToken = IERC20(_usdtToken);
         crvToken = IERC20(_crvToken);
-        exchangeRate = 10;  // 1 ETH = 10 CRV (for example)
+        exchangeRate = 1;  // 1 USDT = 1 CRV (for example)
         rewardRate = _rewardRate;
         owner = msg.sender;
     }
@@ -34,42 +36,42 @@ contract StakingContract {
         _;
     }
 
-    function stake() external payable {
-        require(msg.value > 0, "Amount must be greater than zero.");
+    function stake(uint256 usdtAmount) external {
+        require(usdtAmount > 0, "Amount must be greater than zero.");
 
-        uint256 amount = msg.value;
-        crvToken.transfer(msg.sender, amount * exchangeRate);
+        usdtToken.transferFrom(msg.sender, address(this), usdtAmount);
 
-        totalStakedETH += amount;
-        stakes[msg.sender] = Stake(amount, block.timestamp);
+        totalStakedUSDT += usdtAmount;
+        crvToken.transfer(msg.sender, usdtAmount * exchangeRate);
+        stakes[msg.sender] = Stake(usdtAmount, block.timestamp);
 
-        emit Staked(msg.sender, amount);
+        emit Staked(msg.sender, usdtAmount);
     }
 
     function unstake() external {
         Stake storage stakeInfo = stakes[msg.sender];
-        require(stakeInfo.amount > 0, "No staked ETH.");
+        require(stakeInfo.usdtAmount > 0, "No staked USDT.");
 
-        uint256 amount = stakeInfo.amount;
+        uint256 usdtAmount = stakeInfo.usdtAmount;
         uint256 reward = calculateReward(msg.sender);
 
         delete stakes[msg.sender];
-        crvToken.transferFrom(owner, address(this), amount * exchangeRate);
-        payable(msg.sender).transfer(amount);
+        crvToken.transferFrom(owner, address(this), usdtAmount * exchangeRate);
+        usdtToken.transfer(msg.sender, usdtAmount);
 
-        totalStakedETH -= amount;
+        totalStakedUSDT -= usdtAmount;
 
-        emit Unstaked(msg.sender, amount);
+        emit Unstaked(msg.sender, usdtAmount);
         emit RewardClaimed(msg.sender, reward);
     }
 
     function calculateReward(address account) public view returns (uint256) {
         Stake storage stakeInfo = stakes[account];
-        require(stakeInfo.amount > 0, "No staked ETH.");
+        require(stakeInfo.usdtAmount > 0, "No staked USDT.");
 
         uint256 stakingTime = block.timestamp - stakeInfo.timestamp;
         uint256 rewardPercentage = rewardRate * stakingTime / ONE_WEEK;
-        uint256 rewardAmount = stakeInfo.amount * rewardPercentage / 100;
+        uint256 rewardAmount = stakeInfo.usdtAmount * rewardPercentage / 100;
 
         return rewardAmount;
     }
@@ -82,10 +84,6 @@ contract StakingContract {
         crvToken.transfer(msg.sender, reward);
 
         emit RewardClaimed(msg.sender, reward);
-    }
-
-    function updateExchangeRate(uint256 rate) external onlyOwner {
-        exchangeRate = rate;
     }
 
     function updateRewardRate(uint256 rate) external onlyOwner {
