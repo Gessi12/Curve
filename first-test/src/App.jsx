78e73f2882dd1d82b27f72875eec09c7f11b5238
Gessi12 <2846141630@qq.com>
2024-04-09 17:39:10 +0800

    update react

--- first-test/src/App.jsx
+++ first-test/src/App.jsx
@@ -1,136 +1,144 @@
 import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import USDTStakingContract from './contracts/USDTStakingContract.json';
-import deploymentInfo from "./contracts/deployment.json";
+import TetherToken from './contracts/TetherToken.json';
+import StakeInfo from './contracts/deployment.json';
+import USDTInfo from "./contracts/USDT.json"
 
 const App = () => {
   const [web3, setWeb3] = useState(null);
-  const [contract, setContract] = useState(null);
+  const [stakeContract, setStakeContract] = useState(null);
+  const [usdtContract, setUsdtContract] = useState(null);
   const [account, setAccount] = useState('');
-  const [usdtBalance, setUsdtBalance] = useState(0);
-  const [totalStakedUSDT, setTotalStakedUSDT] = useState('');
+  const [usdtBalance, setUsdtBalance] = useState('');
+  const [totalStakedUSDT, setTotalStakedUSDT] = useState(0);
   const [stakeAmount, setStakeAmount] = useState('');
   const [unstakeAmount, setUnstakeAmount] = useState('');
-  // const [stakingInProgress, setStakingInProgress] = useState(false);
-  // const [unstakingInProgress, setUnstakingInProgress] = useState(false);
   const [pendingReward, setPendingReward] = useState('');
-  const [reward, setReward] = useState(0);
-  // const [error, setError] = useState('');
 
   useEffect(() => {
     initializeWeb3();
   }, []);
 
   const initializeWeb3 = async () => {
-    if (window.ethereum) {
-      try {
-        const web3Instance = new Web3(window.ethereum);
-        setWeb3(web3Instance);
-        loadContract(web3Instance);
-        loadAccount();
-        loadTotalStakedUSDT();
-        loadUsdtBalance();
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      console.error('Please install MetaMask');
+  if (window.ethereum) {
+    try {
+      const web3Instance = new Web3(window.ethereum);
+      setWeb3(web3Instance);
+      await loadUSDTContract(web3Instance); // 确保先加载 USDT 合约
+      await loadStakeContract(web3Instance);
+      await loadAccount(web3Instance);
+      await loadUsdtBalance();
+    } catch (error) {
+      console.error(error);
     }
-  };
+  } else {
+    console.error('Please install MetaMask');
+  }
+};
 
-  const loadAccount = async () => {
-    const accounts = await web3.eth.getAccounts();
+
+  const loadAccount = async (web3Instance) => {
+    const accounts = await web3Instance.eth.getAccounts();
     setAccount(accounts[0]);
   };
 
-  const loadContract = async (web3Instance) => {
+  const loadStakeContract = async (web3Instance) => {
     try {
       const networkId = await web3Instance.eth.net.getId();
-      const contractAddress = deploymentInfo.networks[networkId].address;
+      const contractAddress = StakeInfo[networkId].address;
       const contractInstance = new web3Instance.eth.Contract(
         USDTStakingContract.abi,
-        contractAddress
+        contractAddress,
       );
-      setContract(contractInstance);
-    } catch (error) {
+      setStakeContract(contractInstance);
+    } catch (error){
       console.error(error);
     }
   };
 
-  const loadUsdtBalance = async () => {
+  const loadUSDTContract = async (web3Instance) => {
     try {
-      const usdtBalance = await contract.methods.balanceOf(account).call({ from: account });
-      setUsdtBalance(usdtBalance);
+      const networkId = await web3Instance.eth.net.getId();
+      const contractAddress = USDTInfo[networkId].address;
+      const contractInstance = new web3Instance.eth.Contract(
+        TetherToken.abi,
+        contractAddress,
+      );
+      
+      setUsdtContract(contractInstance);
     } catch (error) {
-      throw new Error('Failed to update USDT balance.');
+      console.error(error);
     }
   };
-
-  const loadTotalStakedUSDT = async () => {
-    const totalStaked = await contract.methods.totalStaked().call();
-    setTotalStakedUSDT(totalStaked);
+    
+  const loadUsdtBalance = async () => {
+    if (usdtContract) { // 检查 usdtContract 是否已经初始化
+      try {
+        const balance = await usdtContract.methods.balanceOf(account).call();
+        setUsdtBalance(balance);
+      } catch (error) {
+        console.error('Failed to load USDT balance:', error);
+      }
+    } else {
+      console.warn('usdtContract is null or undefined. Skipping loading USDT balance.');
+    }
   };
+  
+  
+
+
+  // const loadTotalStakedUSDT = async () => {
+  //   if (stakeContract) { // 检查 stakeContract 是否已经初始化
+  //     try {
+  //       const totalStaked = await stakeContract.methods.totalStakedUSDT().call();
+  //       setTotalStakedUSDT(totalStaked);
+  //     } catch (error) {
+  //       console.error('Failed to load total staked USDT:', error);
+  //     }
+  //   } else {
+  //     console.warn('stakeContract is null or undefined. Skipping loading total staked USDT.');
+  //   }
+  // };
+  
 
   const connectWallet = async () => {
     try {
-      if (window.ethereum) {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        loadAccount();
-        loadUsdtBalance();
-      }
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await loadAccount(web3);
+      
     } catch (error) {
       console.error('Failed to connect wallet:', error);
     }
   };
 
-
   const stake = async () => {
     try {
-      // setStakingInProgress(true);
       const amountToStake = parseFloat(stakeAmount);
-      // const amount = web3.utils.toWei(amountToStake.toString);
-      // const weiAmountToStake = web3.utils.toWei(amountToStake.toString(), 'ether');
-      const transaction = contract.methods.stake().send({
-        from: account,
-        value: amountToStake
-      });
-      // await contract.methods.stake(amount).send({ from: account, value: 0 });
-
-      const receipt = await transaction;
-      console.log('Stake transaction receipt:', receipt);
-      loadTotalStakedUSDT();
-      loadUsdtBalance();
+      const AmountToStake = amountToStake.toString();
+      await stakeContract.methods.stake(AmountToStake).send({ from: account });
+      console.log('Stake successful');
+      await loadUsdtBalance();
     } catch (error) {
-      console.error(error);
-    // } finally {
-    //   setStakingInProgress(false);
+      console.error('Stake failed:', error);
     }
   };
 
   const unstake = async () => {
     try {
-      // setUnstakingInProgress(true);
       const amountToUnstake = parseFloat(unstakeAmount);
-      // await contract.methods.unstake().send({ from: account, value: 0 });
-      const transaction = contract.methods.unstake(amountToUnstake).send({
-        from: account
-      });
-      const receipt = await transaction;
-      console.log('Unstake transaction receipt:', receipt);
-      loadTotalStakedUSDT();
-      loadUsdtBalance();
+      const amount = amountToUnstake.toString();
+      await stakeContract.methods.unstake(amount).send({ from: account });
+      await loadUsdtBalance();
     } catch (error) {
-      console.error(error);
-      // setError('Failed to unstake.');
-    // } finally {
-    //   setUnstakingInProgress(false);
+      console.error('Failed to unstake:', error);
     }
   };
 
   const calculateReward = async () => {
     try {
-      const reward = await contract.methods.calculateReward().call({ from: account });
-      setPendingReward(reward);
+      const reward = await stakeContract.methods.calculateReward(account).call();
+      setPendingReward(web3.utils.fromWei(reward, 'ether'));
     } catch (error) {
       console.error('Failed to calculate reward:', error);
     }
@@ -138,59 +146,56 @@ const App = () => {
 
   const claimReward = async () => {
     try {
-      const transaction = contract.methods.claimReward().send({ from: account });
-      const receipt = await transaction;
-      console.log('Claim reward transaction receipt:', receipt);
+      await stakeContract.methods.claimReward().send({ from: account });
       setPendingReward('');
-      loadUsdtBalance();
+      await loadUsdtBalance();
     } catch (error) {
-      console.error(error);
-      // setError('Failed to claim reward.');
+      console.error('Failed to claim reward:', error);
     }
   };
 
   return (
     <div>
-      <h1>Staking App</h1>
+      <h1>USDT Staking App</h1>
       {account ? (
         <div>
           <p>Connected Account: {account}</p>
-          <p>Account Balance: {usdtBalance}</p>
+          <p>USDT Balance: {usdtBalance}</p>
+        
         </div>
       ) : (
         <button onClick={connectWallet}>Connect Wallet</button>
+        
       )}
-      <p>Total Staked ETH: {totalStakedUSDT}</p>
+      
+      
+      {/* <p>Total Staked USDT: {totalStakedUSDT}</p> */}
       <div>
         <h2>Stake</h2>
-        <input
-          type="number"
-          placeholder="Stake Amount"
-          value={stakeAmount}
-          onChange={(e) => setStakeAmount(e.target.value)}
+        <input type="number" 
+        placeholder="Stake Amount" 
+        value={stakeAmount} 
+        onChange={(e) => setStakeAmount(e.target.value)} 
         />
         <button onClick={stake}>Stake</button>
       </div>
-
+      <div>
+        <h2>Unstake</h2>
+        <input type="number"
+        placeholder="Unstake Amount" 
+        value={unstakeAmount} 
+        onChange={(e) => setUnstakeAmount(e.target.value)} 
+        />
+        <button onClick={unstake}>Unstake</button>
+      </div>
       <div>
         <h2>Calculate Reward</h2>
         <button onClick={calculateReward}>Calculate Reward</button>
-        {pendingReward !== '' && <p>Pending Reward: {pendingReward}</p>}
+        <p>Pending Reward: {pendingReward}</p>
       </div>
-
       <div>
         <h2>Claim Reward</h2>
         <button onClick={claimReward}>Claim Reward</button>
-      </div>
-      <div>
-        <h2>Unstake</h2>
-        <input
-          type="number"
-          placeholder="Unstake Amount"
-          value={unstakeAmount}
-          onChange={(e) => setUnstakeAmount(e.target.value)}
-        />
-        <button onClick={unstake}>Unstake</button>
       </div>
     </div>
   );
